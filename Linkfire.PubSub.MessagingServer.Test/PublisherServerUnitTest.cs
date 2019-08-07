550a2e4ca8ace40653336d88c3462bd99a9904e4
michael kamal <michael.kamal@ibtikar.net.sa>
2019-08-07 21:28:39 +0200

    - Add Tests Folder - Add xUnit Test Project for Messaging Server

---
+++ Linkfire.PubSub.MessagingServer.Test/PublisherServerUnitTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Linkfire.PubSub.MessagingServer.Test
+{
+    public class PublisherServerUnitTest
+    {
+        [Fact]
+        public void CreateWithServerIP()
+        {
+
+        }
+    }
+}
